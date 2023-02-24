cddd312e750c027578aca67dad9c9eeee94aa108
Luis Angel Hernández <jacklu.hdz@gmail.com>
2023-02-24 16:44:05 -0600

    Add new component to add new expense Usage of state with hooks Usage of "bottom-up" state

--- 04. React State and Event Handlers/react-complete-guide/src/components/Expense/ExpenseList.jsx
+++ 04. React State and Event Handlers/react-complete-guide/src/components/Expense/ExpenseList.jsx
@@ -1,18 +1,32 @@
+import { useState } from 'react'
+
 import Card from '../UI/Card'
 import ExpenseItem from './ExpenseItem'
+import ExpensesFilter from './ExpensesFilter'
 import './ExpenseList.css'
 
 const ExpenseList = ({ expenses }) => {
+  const [yearFilter, setYearFilter] = useState('2020')
+
+  const yearSelectionHandler = (year) => {
+    setYearFilter(Number(year))
+  }
+
   return (
     <Card className='expenses'>
+      <ExpensesFilter selectedYear={yearFilter} onYearSelection={yearSelectionHandler} />
       {
-        expenses.map(expense =>
-          <ExpenseItem
-            key={expense.id}
-            date={expense.date}
-            amount={expense.amount}
-            title={expense.title}
-          />)
+        expenses
+          .filter(expense => {
+            return yearFilter === 0 || expense.date.getFullYear() === yearFilter
+          })
+          .map(expense =>
+            <ExpenseItem
+              key={expense.id}
+              date={expense.date}
+              amount={expense.amount}
+              title={expense.title}
+            />)
       }
     </Card>
   )
