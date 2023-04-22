53e3a3970959b873779c6831f1f89b7fb3762a1b
Luis Angel Hernández <jacklu.hdz@gmail.com>
2023-04-21 19:52:45 -0600

    Add post call to add new movies

--- 14. Sending Http Requests/src/App.jsx
+++ 14. Sending Http Requests/src/App.jsx
@@ -2,36 +2,76 @@ import React, { useEffect, useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
+import AddMovie from './components/AddMovie';
+
+const BACKED_ENDPOINT = "https://react-http-c1c44-default-rtdb.firebaseio.com/movies.json"
 
 function App() {
   const [movies, setMovies] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
 
   const fetchMoviesHandler = () => {
-    fetch("https://swapi.py4e.com/api/films/")
-      .then(res => res.json())
+    setError(null)
+    setIsLoading(true)
+    fetch(BACKED_ENDPOINT)
       .then(res => {
-        setMovies(res.results.map(m => {
-          return { 
-            id: m.episode_id,
-            title: m.title,
-            releaseDate: m.release_date,
-            openingText: m.opening_crawl
-          }
-        }))
+        if(!res.ok) {
+          throw new Error('Something went wrong!')
+        }
+        return res.json()
       })
+      .then(data => {
+        const loadedMovies = []
+
+        for (const key in data) {
+          loadedMovies.push({
+            id: key,
+            title: data[key].title,
+            openingText: data[key].openingText,
+            releaseDate: data[key].releaseDate
+          })
+
+        }
+
+        setMovies(loadedMovies)
+      })
+      .catch(err => setError(err.message))
+      .finally(() => setIsLoading(false))
   }
 
   useEffect(() => {
     fetchMoviesHandler()
   }, [])
 
+  function addMovieHandler(movie) {
+    fetch(BACKED_ENDPOINT,
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(res => res.json())
+      .then(res => console.log(res))
+      .catch(err => setError(err))
+  }
+
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {
+          isLoading ? 
+          <div>Loading</div> 
+          : <MoviesList movies={movies} />
+        }
+        {
+          !isLoading && error && <p>{error}</p>
+        }
       </section>
     </React.Fragment>
   );
