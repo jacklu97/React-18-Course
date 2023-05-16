e3558f21321e2dda3b9f0703bca3bd09c5bf5ab6
Luis Angel Hernández <jacklu.hdz@gmail.com>
2023-05-16 15:18:41 -0600

    Set useEffect to send cart data to endpoint

--- 19. Advanced Redux/src/App.js
+++ 19. Advanced Redux/src/App.js
@@ -1,17 +1,79 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+import { uiActions } from "./store/ui-slice";
+
+let isInitial = true;
 
 function App() {
-  const showCart = useSelector(({ui}) => ui.cartIsVisible)
+  const showCart = useSelector(({ ui }) => ui.cartIsVisible);
+  const notification = useSelector(({ ui }) => ui.notification);
+  const cart = useSelector(({ cart }) => cart);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+    );
+
+    fetch("https://react-http-c1c44-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Sending cart data failed");
+        }
+        response.json();
+      })
+      .then(() => {
+        dispatch(
+          uiActions.showNotification({
+            status: "success",
+            title: "Success!",
+            message: "Success!",
+          })
+        );
+      })
+      .catch(() => {
+        dispatch(
+          uiActions.showNotification({
+            status: "error",
+            title: "Error!",
+            message: "Sending cart data failed!",
+          })
+        );
+      });
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          message={notification.message}
+          title={notification.title}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
