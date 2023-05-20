3a7488b993733c721a7c94210fa982f5dfb2be99
Luis Angel Hernández <jacklu.hdz@gmail.com>
2023-05-19 18:47:43 -0600

    Added advanced project

---
+++ 20. Building a Multi-Page SPA/Starting project/src/pages/Home.js
@@ -0,0 +1,24 @@
+import { Link, useNavigate } from "react-router-dom";
+
+const Home = () => {
+  const navigate = useNavigate()
+
+  // Navigation in a programmatically way
+  const navigateHandler = () => {
+    navigate("/products")
+  }
+
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <p>
+        Go to <Link to="/products"> the list of products</Link>
+      </p>
+      <p>
+        <button onClick={navigateHandler} >Navigate</button>
+      </p>
+    </>
+  );
+};
+
+export default Home;
