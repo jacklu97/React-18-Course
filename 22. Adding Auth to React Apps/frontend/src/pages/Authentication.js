f263ef4b9b505ce791846f69d76f16c6ac1b46f4
Luis Angel Hernández <jacklu.hdz@gmail.com>
2023-06-27 17:26:00 -0600

    Add user login route Add auth action Enhance auth page by using query params

--- 22. Adding Auth to React Apps/frontend/src/pages/Authentication.js
+++ 22. Adding Auth to React Apps/frontend/src/pages/Authentication.js
@@ -1,7 +1,48 @@
-import AuthForm from '../components/AuthForm';
+import { json, redirect } from "react-router-dom";
+
+import AuthForm from "../components/AuthForm";
 
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
 export default AuthenticationPage;
+
+const SUPPORTED_MODES = {
+  SIGNUP: "signup",
+  LOGIN: "login",
+};
+
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get("mode") ?? "login";
+
+  if (!Object.values(SUPPORTED_MODES).includes(mode)) {
+    throw json({ message: "Unsupported mode." }, { status: 422 });
+  }
+
+  const data = await request.formData();
+  const authData = {
+    email: data.get("email"),
+    password: data.get("password"),
+  };
+
+  const response = await fetch("http://localhost:8080/" + mode, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(authData),
+  });
+
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw json({ message: "Could not authenticate user" }, { status: 500 });
+  }
+
+  // soon: manage token
+  return redirect('/')
+}
